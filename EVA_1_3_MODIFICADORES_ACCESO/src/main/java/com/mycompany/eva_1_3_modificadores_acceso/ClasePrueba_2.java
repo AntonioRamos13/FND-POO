24a8167501e588681f7fd5853d52bf4262c011f8
AntonioRamos13 <143569091+AntonioRamos13@users.noreply.github.com>
2024-05-22 16:54:27 -0600

    Add files via upload

---
+++ EVA_1_3_MODIFICADORES_ACCESO/src/main/java/com/mycompany/eva_1_3_modificadores_acceso/ClasePrueba_2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.eva_1_3_modificadores_acceso;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ClasePrueba_2 {
+      private int w;
+    protected int x;
+    public int y;
+    int z;
+    
+}
---
+++ EVA_1_3_MODIFICADORES_ACCESO/src/main/java/com/mycompany/eva_1_3_modificadores_acceso/EVA_1_3_MODIFICADORES_ACCESO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_1_3_modificadores_acceso;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA_1_3_MODIFICADORES_ACCESO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
