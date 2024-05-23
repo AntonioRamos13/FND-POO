0346134d6d6ad962238cd25ed90929cc2964fc27
AntonioRamos13 <143569091+AntonioRamos13@users.noreply.github.com>
2024-05-23 07:22:49 -0600

    Add files via upload

---
+++ EVA2_1_SOBRECARGA_METODOS/src/main/java/com/mycompany/eva2_1_sobrecarga_metodos/EVA2_1_SOBRECARGA_METODOS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_1_sobrecarga_metodos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_1_SOBRECARGA_METODOS {
+
+    public static void main(String[] args) {
+        System.out.println("La suma 5 + 4 = " + sumar(5,4));
+        System.out.println("La suma 5.3 + 4.1 = " + sumar(5.3,4.1));
+        System.out.println("La suma Hola + Mundo = " + sumar("Hola","Mundo"));
+    }
+    
+    public static int sumar(int num1, int num2){
+    
+    return num1 + num2;
+    
+    }
+     public static int sumar(double num1, double num2){
+    
+    return (int) (num1 + num2);
+    
+    }
+     public static String sumar(String num1, String num2){
+    
+    return num1 + num2;
+    
+    }
+}
